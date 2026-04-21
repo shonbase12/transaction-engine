e47e7bdd5a32100758797779848321cb3549b8dd
shonbase12 <shonbase12@gmail.com>
2026-04-21 15:15:26 +0300

    Added batch save operation to TransactionRepository interface

--- src/TransactionRepository.java
+++ src/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.novapay.transactions;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TransactionRepository {
     void save(Transaction tx);
+    void saveAll(List<Transaction> transactions);
     Optional<Transaction> findById(String txId);
     void updateState(String txId, TransactionState state);
 }
